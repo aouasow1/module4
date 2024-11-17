c2bf27eaf2b7398ace515904e3a820374d9be0b3
aouasow1 <asow7@ivytech.edu>
2024-11-17 16:32:02 -0500

    Update sow6.py

--- sow6.py
+++ sow6.py
@@ -1,3 +1,4 @@
+#16.8 Use the sqlalchemy module to connect to the sqlite3 database books.db that you just made in exercise 16.4.
 import sqlalchemy
 conn = sqlalchemy.create_engine('sqlite:///books.db')
 sql = 'select title from book order by title asc'
